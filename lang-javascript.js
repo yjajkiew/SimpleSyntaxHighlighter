e064a127f673f6f08995b82495cf571ec52a9865
Yann Jajkiewicz <contact@yann.me>
2014-02-16 20:03:53 +0100

    project start

---
+++ lang-javascript.js
@@ -0,0 +1,15 @@
+var delimiter = ";";
+
+var commentsStart = new Array("/*", "//");
+var commentsEnd = new Array("*/", "\\n");
+
+var keywords =	'break case catch class continue ' +
+				'default delete do else enum export extends false  ' +
+				'for function if implements import in instanceof ' +
+				'interface let new null package private protected ' +
+				'static return super switch ' +
+				'this throw true try typeof var while with yield';
+
+var functions = "count length";
+
+var types = "Object boolean";
